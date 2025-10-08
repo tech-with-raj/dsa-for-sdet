4def551e14d0d284f490d9d03fa45499eea453c2
Rajasekar K <rajasekar.k0703@gmail.com>
2025-10-08 08:15:58 +0530

    Implemented logic for CheckMonotonicArray

---
+++ src/main/java/LogicBuilding/CheckMonotonicArray.java
@@ -0,0 +1,36 @@
+package main.java.LogicBuilding;
+
+public class CheckMonotonicArray {
+
+    public static void main(String[] args) {
+
+        int[] a = {2, 3, 6, 8, 9};
+
+        boolean increasing = true;
+        boolean decreasing = true;
+
+        for (int i = 1; i < a.length; i++) {
+
+            if (a[i - 1] < a[i]) {
+
+                decreasing = false;
+
+            } else if (a[i - 1] > a[i]) {
+
+                increasing = false;
+            }
+
+        }
+
+        if (increasing || decreasing) {
+
+            System.out.println("The given array is Monotonic");
+
+        } else {
+
+            System.out.println("The given array is not Monotonic");
+
+        }
+
+    }
+}
