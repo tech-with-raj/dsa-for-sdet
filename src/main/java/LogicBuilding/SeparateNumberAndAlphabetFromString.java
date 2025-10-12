c1e5c8cc9e67f1f33bf21da427e22190a7d75b04
Rajasekar K <rajasekar.k0703@gmail.com>
2025-10-12 10:51:05 +0530

    Implemented logic for SeparateNumberAndAlphabetFromString

---
+++ src/main/java/LogicBuilding/SeparateNumberAndAlphabetFromString.java
@@ -0,0 +1,31 @@
+package main.java.LogicBuilding;
+
+public class SeparateNumberAndAlphabetFromString {
+
+    public static void main(String[] args) {
+
+        String input = "Ra123j";
+
+        char[] charArray = input.toCharArray();
+        StringBuilder alphabet = new StringBuilder();
+        StringBuilder number = new StringBuilder();
+
+        for (char currentChar : charArray) {
+
+            if (Character.isDigit(currentChar)) {
+
+                number.append(currentChar);
+
+            } else if (Character.isAlphabetic(currentChar)) {
+
+                alphabet.append(currentChar);
+            }
+
+        }
+
+        System.out.println("Alphabets from String " + input + " are " + alphabet.toString());
+        System.out.println("Number from String " + input + " is " + number.toString());
+
+    }
+
+}
