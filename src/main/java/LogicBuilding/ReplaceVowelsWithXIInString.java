b03ade382facaa1af104a0d1e6d44fbcd8d7c508
Rajasekar K <rajasekar.k0703@gmail.com>
2025-10-13 09:35:55 +0530

    Implemented logic for ReplaceVowelsWithXIInString

---
+++ src/main/java/LogicBuilding/ReplaceVowelsWithXIInString.java
@@ -0,0 +1,47 @@
+package main.java.LogicBuilding;
+
+public class ReplaceVowelsWithXIInString {
+
+    public static void main(String[] args) {
+
+        String name = "Rajasekar";
+
+        char[] inputArray = name.toCharArray();
+
+        StringBuilder finalName = new StringBuilder();
+
+        for (char currentCharacter : inputArray) {
+
+            if(isVowel(currentCharacter))
+            {
+
+                finalName.append('x');
+
+            }else{
+
+                finalName.append(currentCharacter);
+            }
+
+        }
+
+        System.out.println(finalName);
+
+    }
+
+
+    public static boolean isVowel(char currentCharacter) {
+
+        if (currentCharacter == 'a' || currentCharacter == 'e' || currentCharacter == 'i'
+                || currentCharacter == 'o' || currentCharacter == 'u' || currentCharacter == 'A' || currentCharacter == 'E' || currentCharacter == 'I'
+                || currentCharacter == 'O' || currentCharacter == 'U') {
+
+            return true;
+
+        } else {
+
+            return false;
+        }
+
+    }
+
+}
