77b136a7c3cbe995d3210ace1a11482628886840
Rajasekar K <rajasekar.k0703@gmail.com>
2025-10-30 09:13:16 +0530

    Implemented logic for EvenOrOddFromArrayList

---
+++ src/main/java/LogicBuilding/EvenOrOddFromArrayList.java
@@ -0,0 +1,43 @@
+package main.java.LogicBuilding;
+
+import java.util.ArrayList;
+
+
+public class EvenOrOddFromArrayList {
+
+
+    public static void main(String[] args) {
+
+        ArrayList<Object> arrayList = new ArrayList<>();
+
+        arrayList.add("Java");
+        arrayList.add(24);
+        arrayList.add("Raj");
+        arrayList.add(23);
+        arrayList.add(21.98);
+
+
+        for (Object O : arrayList) {
+
+            if (O instanceof Integer) {
+
+                if (((Integer) O) % 2 == 0) {
+
+                    System.out.println("Even value " + O);
+                } else {
+
+                    System.out.println("odd value " + O);
+
+                }
+
+
+            }
+
+
+        }
+
+
+    }
+
+
+}
