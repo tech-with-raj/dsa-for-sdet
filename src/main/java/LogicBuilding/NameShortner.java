04d35ed81d9af5d5f520189a17ca95974d03d2cc
Rajasekar K <rajasekar.k0703@gmail.com>
2025-11-05 20:24:22 +0530

    Implemented logic for NameShortner

---
+++ src/main/java/LogicBuilding/NameShortner.java
@@ -0,0 +1,25 @@
+package main.java.LogicBuilding;
+
+public class NameShortner {
+
+    public static void main(String[] args){
+
+        String fullName = "Rajasekar Muthu Subramanian";
+
+        String[] nameArray =fullName.split(" ");
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int index =0;  index < nameArray.length-1; index++){
+
+            sb.append(nameArray[index].charAt(0));
+            sb.append(".");
+        }
+
+        sb.append(nameArray[nameArray.length-1]);
+
+        System.out.println("Shorter name of "+fullName+" is "+sb);
+
+    }
+
+}
