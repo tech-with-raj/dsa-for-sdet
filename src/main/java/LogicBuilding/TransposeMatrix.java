2f91e1d690d67cc6d9c2cb2d164932936ba00e70
Rajasekar K <rajasekar.k0703@gmail.com>
2025-11-06 08:16:55 +0530

    Implemented logic for TransposeMatrix

---
+++ src/main/java/LogicBuilding/TransposeMatrix.java
@@ -0,0 +1,48 @@
+package main.java.LogicBuilding;
+
+public class TransposeMatrix {
+
+    public static void main(String[] args) {
+
+        int[][] a = new int[2][3];
+
+        a[0][0] = 1;
+        a[0][1] = 2;
+        a[0][2] = 3;
+        a[1][0] = 4;
+        a[1][1] = 5;
+        a[1][2] = 6;
+
+        int[][] result = new int[3][2];
+
+
+        for (int row = 0; row < a.length; row++) {
+
+            for (int col = 0; col < a[0].length; col++) {
+
+                System.out.print(a[row][col] + " ");
+            }
+            System.out.println("");
+        }
+
+        for (int row = 0; row < a.length; row++) {
+
+            for (int col = 0; col < a[0].length; col++) {
+
+                result[col][row] = a[row][col];
+            }
+
+        }
+
+        for (int row = 0; row < result.length; row++) {
+
+            for (int col = 0; col < result[0].length; col++) {
+
+                System.out.print(result[row][col] + " ");
+            }
+            System.out.println("");
+        }
+
+
+    }
+}
