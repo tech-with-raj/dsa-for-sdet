96d19a37dfe7805139a9968c59414884baa8c8cd
Rajasekar K <rajasekar.k0703@gmail.com>
2025-12-31 17:54:36 +0530

    Implemented logic for FindLengthOfString2

---
+++ src/main/java/LogicBuilding/FindLengthOfString2.java
@@ -0,0 +1,31 @@
+package main.java.LogicBuilding;
+
+import java.awt.*;
+
+public class FindLengthOfString2 {
+
+    public static void main(String[] args) {
+
+        String input = "Hello Rajasekar";
+
+        int count = 0;
+
+        while (true) {
+
+            try {
+
+                input.charAt(count);
+                count++;
+
+            } catch (Exception e) {
+
+                System.out.println(count);
+                break;
+            }
+
+        }
+
+
+    }
+
+}
