cb6c9cc44142dc2cdb67377f4da0747372f8ae3e
Rajasekar K <rajasekar.k0703@gmail.com>
2026-01-28 21:19:36 +0530

    Implemented LinkedListRunner and Node

---
+++ src/main/java/LogicBuilding/LinkedListRunner.java
@@ -0,0 +1,44 @@
+package main.java.LogicBuilding;
+
+
+public class LinkedListRunner {
+
+    public static void main(String[] args) {
+
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+
+        traverseLinkedList(head);
+        traverseLinkedList(reverseLinkedList(head));
+    }
+
+    public static void traverseLinkedList(Node head) {
+
+        System.out.println("");
+        Node current = head;
+
+        while (current != null) {
+            System.out.print(current.getData() + " ");
+            current = current.next;
+        }
+    }
+
+    public static Node reverseLinkedList(Node head) {
+
+        Node current = head;
+        Node prev = null;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
+
