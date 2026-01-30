97bfda5027c48f7f098812e8818fac7c61cde3c7
Rajasekar K <rajasekar.k0703@gmail.com>
2026-01-30 15:34:33 +0530

    Implement logic for RemoveDuplicatesElementsFromArray

---
+++ src/main/java/LogicBuilding/RemoveDuplicatesElementsFromArray.java
@@ -0,0 +1,30 @@
+package main.java.LogicBuilding;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+public class RemoveDuplicatesElementsFromArray {
+
+    public static void main(String[] args) {
+
+        int[] inputArray = {1, 2, 2, 3, 3, 4, 5};
+
+        LinkedHashSet<Integer> uniqueElements = new LinkedHashSet<>();
+
+        for (int i : inputArray) {
+
+            uniqueElements.add(i);
+        }
+
+        System.out.println(uniqueElements);
+
+        // Converting Set to Array
+
+        int[] result =uniqueElements.stream().mapToInt(Integer::intValue).toArray();
+
+        System.out.println(Arrays.toString(result));
+
+    }
+
+
+}
