afa1d212bfc80fbf422d53eae7809269d37f74df
Rajasekar K <rajasekar.k0703@gmail.com>
2026-02-04 21:05:09 +0530

    Implement logic for CountLowerCaseAndUpperCaseCharacters

---
+++ src/main/java/LogicBuilding/CountLowerCaseAndUpperCaseCharacters.java
@@ -0,0 +1,34 @@
+package main.java.LogicBuilding;
+
+public class CountLowerCaseAndUpperCaseCharacters {
+
+    public static void main(String[] args){
+
+        String input = "aGskdSrikdkdSjkdSDGE";
+
+        char[] inputArray =input.toCharArray();
+
+        int upperCaseCount = 0;
+        int lowerCaseCount = 0;
+
+
+        for(char letter : inputArray){
+
+            if(Character.isUpperCase(letter)){
+
+                upperCaseCount++;
+
+            }else if(Character.isLowerCase(letter)){
+
+                lowerCaseCount++;
+            }
+
+        }
+
+        System.out.println("UpperCase count is "+upperCaseCount);
+        System.out.println("LowerCaseCount is "+lowerCaseCount);
+
+    }
+
+
+}
