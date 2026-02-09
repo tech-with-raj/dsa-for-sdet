0e7b8fc7819af69ffd51a9e6aa3fe09f530aab9d
Rajasekar K <rajasekar.k0703@gmail.com>
2026-02-09 18:41:14 +0530

    Implement logic for FindTotalNumberOfCharacterWithoutWhiteSpace

---
+++ src/main/java/LogicBuilding/FindTotalNumberOfCharacterWithoutWhiteSpace.java
@@ -0,0 +1,35 @@
+package main.java.LogicBuilding;
+
+public class FindTotalNumberOfCharacterWithoutWhiteSpace {
+
+    public static void main(String[] args) {
+
+        String str = " hello world raj me";
+
+        char[] strArray = str.toCharArray();
+
+        int count = 0;
+
+        for (int index = 0; index < strArray.length; index++) {
+
+            if (strArray[index] == ' ') {
+
+                continue;
+            }
+
+            boolean spaceBeforeChar = ((index > 0) && (strArray[index - 1] == ' '));
+            boolean spaceAfterChar = ((index < str.length()-1) && (strArray[index + 1] == ' '));
+
+            if(!spaceBeforeChar && !spaceAfterChar){
+
+                count++;
+            }
+
+
+        }
+
+         System.out.println("The total number of Character without white space before and after it is "+count);
+
+    }
+
+}
